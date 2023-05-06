981116b32d1a03fb4b00276ffef6b3a7bf33afde
alihennous <57763534+alihennous@users.noreply.github.com>
2023-05-06 17:07:14 +0100

    Update vrevervfv.js

--- vrevervfv.js
+++ vrevervfv.js
@@ -7,8 +7,7 @@ $.ajax({
       $('#posts').append('<a class="shadow hover:shadow-lg mb-3 flex-col" href="data:post.url"> <div class="relative pt-[70%] w-full"> <img class="w-full h-full absolute top-0" src="jjtyjtj" alt=""/> <p class=" text-xs absolute top-2 right-2 px-2 py-2 text-[10px] text-white bg-zinc-400/80 dark:text-zinc-100 dark:bg-zinc-700/60"><data:post.timestamp/></p> </div> <div class="px-2 pt-2 pb-4"> <h1 class="leading-[22px] text-[14px]"><data:post.title/></h1> </div> </a>');
     }
 
-  });
-
+ });
 
 
   alert('ffffffffffffffffffffffefef');
